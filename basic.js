5b5fd77e7a41d938cd27f8b5466d520472a43c7e
itsbamme <jan9239@naver.com>
2023-12-22 21:02:44 +0900

    제네릭으로 유니온 타입 지정

--- basic.js
+++ basic.js
@@ -1,30 +1,14 @@
 //인터페이스로 지정된 객체로 구성된 배열 타입 지정 (실무에서 제일 많이 쓰이는 패턴 - api 리턴 값)
-var data = [
+const data = [
     { name: 'Hanna', age: 20, isFemale: true, address: 'Seoul' },
     { name: 'Emily', age: 30, isFemale: false },
     { name: 'Michael', age: 40, isFemale: false }
 ];
-//단순 문자타입으로 구성된 배열타입 지정
-var data2 = ['red', 'green', 'blue'];
-var getArrayInfo = function (arr) {
-    console.log(arr);
-};
-var getTest = function (arr) {
+//해당 인터페이스의 배열값 뿐 아니라 인터페이스 객체 자체도 전달받도록 유니온 타입 지정
+const getInfo = (arr) => {
     console.log(arr);
 };
 //제네릭으로 호출 시 인터페이스 타입 지정해서 호출
-getArrayInfo(data);
-getArrayInfo(data2);
-var custom = [
-    { name: 'Paul', age: '30', isFemale: false },
-    { name: 'Mark', age: 23, isFemale: false }
-];
-//아래와 같이 함수 호출시 Generic으로 지정한 타입과 다른 값이 들어가면 에러발생
-//잠재적인 오류를 미연에 방지 가능 (제네릭을 쓰는 이유)
-getArrayInfo(custom);
-// getArrayInfo(Student[]);
-//아래 코드에서는 숫자값만 담긴 배열이 인수로 넘어가도록 제네릭을 설정했지만
-//문자값이 포함되어 있는 배열을 인수로 전달해서 제네릭이 호출 시 에러 발생
-var custom2 = [1, 2, '3'];
-getArrayInfo(custom2);
-// getArrayInfo(number[])
+getInfo(data);
+//아래처럼 배열이 아닌 student 타입의 객체로 인수로 전달가능
+getInfo(data[0]);
--- basic.ts
+++ basic.ts
@@ -13,35 +13,13 @@ const data: Student[] = [
 	{ name: 'Michael', age: 40, isFemale: false }
 ];
 
-//단순 문자타입으로 구성된 배열타입 지정
-const data2: string[] = ['red', 'green', 'blue'];
-
-const getArrayInfo = <type>(arr: type[]) => {
-	console.log(arr);
-};
-
-const getTest = (arr: number[]) => {
+//해당 인터페이스의 배열값 뿐 아니라 인터페이스 객체 자체도 전달받도록 유니온 타입 지정
+const getInfo = <type>(arr: type[] | type) => {
 	console.log(arr);
 };
 
 //제네릭으로 호출 시 인터페이스 타입 지정해서 호출
-getArrayInfo<Student>(data);
-getArrayInfo<string>(data2);
+getInfo<Student>(data);
 
-const custom = [
-	{ name: 'Paul', age: '30', isFemale: false },
-	{ name: 'Mark', age: 23, isFemale: false }
-];
-
-//아래와 같이 함수 호출시 Generic으로 지정한 타입과 다른 값이 들어가면 에러발생
-//잠재적인 오류를 미연에 방지 가능 (제네릭을 쓰는 이유)
-
-getArrayInfo<Student>(custom);
-// getArrayInfo(Student[]);
-
-//아래 코드에서는 숫자값만 담긴 배열이 인수로 넘어가도록 제네릭을 설정했지만
-//문자값이 포함되어 있는 배열을 인수로 전달해서 제네릭이 호출 시 에러 발생
-
-const custom2 = [1, 2, '3'];
-getArrayInfo<number>(custom2);
-// getArrayInfo(number[])
+//아래처럼 배열이 아닌 student 타입의 객체로 인수로 전달가능
+getInfo<Student>(data[0]);
