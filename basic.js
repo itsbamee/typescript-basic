c5eedfa14526d35b048d7d8f9efd311c6aa4d83f
itsbamme <jan9239@naver.com>
2023-12-22 19:27:07 +0900

    기본 타입 지정

---
+++ basic.js
@@ -0,0 +1,11 @@
+//기본타입
+let text = 'hello';
+let num = 0;
+let isFemale = 'true';
+//참조형 타입
+let like = ['drama', 'music', 'clean'];
+let unlike = ['dirty', 'liar', 'war'];
+let odd = [1, 3, 5];
+let even = [2, 4, 6];
+//tuple: 배열에 들어가는 타입이 다를 때
+let combined = [3, 'clean', false];
---
+++ basic.ts
@@ -0,0 +1,13 @@
+//기본타입
+let text: string = 'hello';
+let num: number = 0;
+let isFemale: boolean = 'true';
+
+//참조형 타입
+let like: string[] = ['drama', 'music', 'clean'];
+let unlike: Array<string> = ['dirty', 'liar', 'war'];
+let odd: number[] = [1, 3, 5];
+let even: number[] = [2, 4, 6];
+
+//tuple: 배열에 들어가는 타입이 다를 때
+let combined: [number, string, boolean] = [3, 'clean', false];
