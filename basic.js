9c3d92b85fbf6c7339c10c09a409d60177983990
itsbamme <jan9239@naver.com>
2023-12-22 20:47:08 +0900

    제네릭으로 인터페이스 타입을 호출단에서 지정해서 호출

--- basic.js
+++ basic.js
@@ -1,22 +1,14 @@
-// const numbers: number[] = [1, 2, 3];
-const numbers = [1, 2, 3];
-const text = ['a', 'b', 'c'];
-//제네릭(Generic)
-//공통된 규칙인데 들어가는 자료형만 바뀔 떄
-//일일이 타입을 따로 지정하는게 비효율적이므로 타입 지정을 호출할 때
-//사용자로 하여금 지정하게 하는 틀
-/*
-  아래 코드의 경우 배열의 갯수값을 구하고 싶을 때
-  똑같은 구조임에도 불구하고 배열에 들어가는 자료형에 타입을 다르게 지정해야 되기 때문에 함수를 또 생성하는 번거로움
-*/
-//아래와 같이 type을 any로 지정하면 사용가능하나
-//any 타입은 우리 type을 지정하지 않았을 때 자동으로 설정되는 값이므로
-//컴파일시 에러를 잡지 못함
-//제네릭은 정의할 때 타입을 지정하는 것이 아닌 런타임에서 호출단계에 타입을 지정하는 구조
-//<타입변수명>
-const getLength = (arr) => {
-    return arr.length;
+//인터페이스로 지정된 객체로 구성된 배열 타입 지정 (실무에서 제일 많이 쓰이는 패턴 - api 리턴 값)
+var data = [
+    { name: 'Hanna', age: 20, isFemale: true, address: 'Seoul' },
+    { name: 'Emily', age: 30, isFemale: false },
+    { name: 'Michael', age: 40, isFemale: false }
+];
+//단순 문자타입으로 구성된 배열타입 지정
+var data2 = ['red', 'green', 'blue'];
+var getArrayInfo = function (arr) {
+    console.log(arr);
 };
-console.log(getLength([1, 2, 3]));
-console.log(getLength(['a', 'b', 'c']));
-console.log(getLength([1, 'b', 'c']));
+//제네릭으로 호출 시 인터페이스 타입 지정해서 호출
+getArrayInfo(data);
+getArrayInfo(data2);
